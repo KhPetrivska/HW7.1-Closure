22915a29ac93f3f524efa2f8bf9c5179dc0a2224
Kristina <kristin.petrivska@gmail.com>
2024-07-26 18:11:48 -0600

    add solution

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+'use strict'; 
 
+const sum = (function () {
+let total = 0;
+function inner (num){
+total += num;
+return total;
+}
+return inner
+})();
+
+
+console.log(sum(4));
+console.log(sum(6));
+console.log(sum(10));
+console.log(sum(7))
